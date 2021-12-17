356ad9d58e68b3b9b8b193462de0a90ebd01a968
AliceSky <3589035402@qq.com>
2021-12-17 14:19:51 +0800

    perf(py_get_dict_from_class_based):subtitude file path for variable

--- py_get_dict_from_class_based.py
+++ py_get_dict_from_class_based.py
@@ -1,4 +1,7 @@
-with open ('D:\TestCode\hclg_testcode\hz2phone.txt','r',encoding='utf-8') as f1 , open ('D:\TestCode\hclg_testcode\corpus_class_based.txt','r',encoding='utf-8') as f2:
+DIR_hz2phone = 'D:\TestCode\hclg_testcode\hz2phone.txt'
+DIR_corpus_class = 'D:\TestCode\hclg_testcode\corpus_class_based.txt'
+DIR_dict = "D:\TestCode\hclg_testcode\dict_py.txt"
+with open (DIR_hz2phone,'r',encoding='utf-8') as f1 , open (DIR_corpus_class,'r',encoding='utf-8') as f2:
     linelist = [l.strip('\n') for l in f1.readlines()] #末尾空格不去除，因为需要多个词的拼音组合
     linelist_2 = [l.strip('\n') for l in f2.readlines()] 
 f1.close()
@@ -8,7 +11,7 @@ dict_hz2phone = dict([tuple(l.split(" ",1)) for l in linelist]) #将hz2phone.txt
 k1_k2_list = [(l.split(" ",2))[:2] for l in linelist_2] #取每行前两个词放入列表
 ci_list = list(set([x for list_outer in k1_k2_list for x in list_outer if "CLASS" not in x ])) #不含CLASS字符的词，去重
 
-with open("D:\TestCode\hclg_testcode\dict_py.txt","a+",encoding='utf-8') as f3: 
+with open(DIR_dict,"a+",encoding='utf-8') as f3: 
     for item in ci_list:
         ci2phone_list = []
         for x in item:
